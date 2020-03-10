5f98dd4d760c95e49b5046bbe5d3075b9526795e
Joshua Eisenmann <thisiseisenmann@gmail.com>
2020-03-09 21:13:28 -0700

    Added babel dependencies, added webpack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+let path = require('path');
+let source_dir = path.join(__dirname, '/client/src');
+let public_dir = path.join(__dirname, '/public');
+
+module.exports = {
+  entry: `${source_dir}/index.jsx`,
+  output: {
+    filename: `bundle.js`,
+    path: `${public_dir}`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: source_dir,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015']
+          }
+        }
+      }
+    ]
+  }
+};
