dad4465c8c28a2ded460b9e08613e520a09c0975
Joshua Eisenmann <thisiseisenmann@gmail.com>
2020-03-14 13:08:04 -0700

    Added barebones for server index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-//express server
+const express = require('express');
+const path = require('path');
+
+let app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  console.log('initial GET request');
+  res.sendStatus(200);
+});
+
+app.listen(3004, () => {
+  console.log('Express server for REVIEWS listening on port 3004');
+});
