155adb0820100e40fdd5ced5f7a68c7974de8dc0
Joshua Eisenmann <thisiseisenmann@gmail.com>
2020-03-26 08:39:54 -0700

    Server unit tests added

--- spec/server.test.js
+++ spec/server.test.js
@@ -1,5 +1,7 @@
 const {MongoClient} = require('mongodb');
 import "regenerator-runtime/runtime";
+const request = require('supertest');
+const app = require('../server/index');
 
 describe('insert', () => {
   let connection;
@@ -14,11 +16,35 @@ describe('insert', () => {
   });
 
 
-  describe('Ratings module: attrScore function', () => {
+  describe('Server tests', () => {
 
-  test('expect attrScore to return a length of 0 if no reviews', () => {
-    expect(0).toBe(0);
+  test('should insert a testuser in collection', async () => {
+    const users = db.collection('users');
+
+    const mockUser = {_id: 'some-user-id', name: 'John'};
+    await users.insertOne(mockUser);
+
+    const insertedUser = await users.findOne({_id: 'some-user-id'});
+    expect(insertedUser).toEqual(mockUser);
   });
+
+  test('should get 200 when looking up listing ID', async () => {
+      await request(app)
+      .get('/10001')
+      .expect(200)
+  });
+
+  test('should get 200 when looking up listing path', async () => {
+    await request(app)
+    .get('/listing')
+    .expect(200)
+});
+
+test('should get string when looking up averageScore10001', async () => {
+  await request(app)
+  .get('/averageScore10001')
+  .expect(200)
+});
 
 });
 
